dce763f7b6477a02c82cc46d22c228ca656064fc
nikolay.elenkov <nikolay.elenkov@74032484-283b-11df-b97d-a14e33d00e86>
2010-03-10 09:51:40 +0000

    better detail views

--- trunk/wwwjdic/src/org/nick/wwwjdic/DictionaryEntryAdapter.java
+++ trunk/wwwjdic/src/org/nick/wwwjdic/DictionaryEntryAdapter.java
@@ -12,64 +12,69 @@ import android.widget.TextView;
 
 public class DictionaryEntryAdapter extends BaseAdapter {
 
-	private final Context context;
-	private final List<DictionaryEntry> entries;
+    private static final int SHORT_TRANSLATION_LENGTH = 40;
 
-	public DictionaryEntryAdapter(Context context, List<DictionaryEntry> entries) {
-		this.context = context;
-		this.entries = entries;
-	}
+    private final Context context;
+    private final List<DictionaryEntry> entries;
 
-	public int getCount() {
-		return entries == null ? 0 : entries.size();
-	}
+    public DictionaryEntryAdapter(Context context, List<DictionaryEntry> entries) {
+        this.context = context;
+        this.entries = entries;
+    }
 
-	public Object getItem(int position) {
-		return entries == null ? null : entries.get(position);
-	}
+    public int getCount() {
+        return entries == null ? 0 : entries.size();
+    }
 
-	public long getItemId(int position) {
-		return position;
-	}
+    public Object getItem(int position) {
+        return entries == null ? null : entries.get(position);
+    }
 
-	public View getView(int position, View convertView, ViewGroup parent) {
-		DictionaryEntry entry = entries.get(position);
+    public long getItemId(int position) {
+        return position;
+    }
 
-		return new DictionaryEntryListView(context, entry);
-	}
+    public View getView(int position, View convertView, ViewGroup parent) {
+        DictionaryEntry entry = entries.get(position);
 
-	private final class DictionaryEntryListView extends LinearLayout {
-		private TextView entryText;
-		private TextView readingText;
-		private TextView translationText;
+        return new DictionaryEntryListView(context, entry);
+    }
 
-		public DictionaryEntryListView(Context context, DictionaryEntry entry) {
-			super(context);
-			setOrientation(LinearLayout.VERTICAL);
-			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-					ViewGroup.LayoutParams.WRAP_CONTENT,
-					ViewGroup.LayoutParams.WRAP_CONTENT);
-			params.setMargins(5, 3, 5, 0);
+    private final class DictionaryEntryListView extends LinearLayout {
+        private TextView entryText;
+        private TextView readingText;
+        private TextView translationText;
 
-			entryText = new TextView(context);
-			entryText.setText(entry.getWord());
-			entryText.setTextSize(20f);
-			entryText.setTextColor(Color.WHITE);
-			addView(entryText, params);
+        public DictionaryEntryListView(Context context, DictionaryEntry entry) {
+            super(context);
+            setOrientation(LinearLayout.VERTICAL);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.setMargins(5, 3, 5, 0);
 
-			if (entry.getReading() != null) {
-				readingText = new TextView(context);
-				readingText.setText(entry.getReading());
-				readingText.setTextSize(18f);
-				readingText.setTextColor(Color.WHITE);
-				addView(readingText, params);
-			}
+            entryText = new TextView(context);
+            entryText.setText(entry.getWord());
+            entryText.setTextSize(20f);
+            entryText.setTextColor(Color.WHITE);
+            addView(entryText, params);
 
-			translationText = new TextView(context);
-			translationText.setText(entry.getShortTranslation());
-			translationText.setTextSize(16f);
-			translationText.setTextColor(Color.WHITE);
-			addView(translationText, params);
-		}
-	}
+            if (entry.getReading() != null) {
+                readingText = new TextView(context);
+                readingText.setText(entry.getReading());
+                readingText.setTextSize(18f);
+                readingText.setTextColor(Color.WHITE);
+                addView(readingText, params);
+            }
+
+            translationText = new TextView(context);
+            String translationStr = StringUtils.join(entry.getMeanings(), "/",
+                    0);
+            translationText.setText(StringUtils.shorten(translationStr,
+                    SHORT_TRANSLATION_LENGTH));
+            translationText.setTextSize(16f);
+            translationText.setTextColor(Color.WHITE);
+            addView(translationText, params);
+        }
+    }
 }
