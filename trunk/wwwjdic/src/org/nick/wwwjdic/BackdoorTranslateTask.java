003a4d69b72f3ebb26a6f50ae07f90d2d36861db
nikolay.elenkov <nikolay.elenkov@74032484-283b-11df-b97d-a14e33d00e86>
2010-03-11 03:33:40 +0000

    properly parse new raw format

--- trunk/wwwjdic/src/org/nick/wwwjdic/BackdoorTranslateTask.java
+++ trunk/wwwjdic/src/org/nick/wwwjdic/BackdoorTranslateTask.java
@@ -2,7 +2,9 @@ package org.nick.wwwjdic;
 
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.http.client.ClientProtocolException;
@@ -10,70 +12,100 @@ import org.apache.http.client.methods.HttpGet;
 
 import android.util.Log;
 
-public abstract class BackdoorTranslateTask extends TranslateTask {
+public abstract class BackdoorTranslateTask<T> extends TranslateTask {
 
-	private static final String BACKDOOR_URL = "http://www.csse.monash.edu.au/~jwb/cgi-bin/wwwjdic.cgi";
+    private static final String BACKDOOR_URL = "http://www.csse.monash.edu.au/~jwb/cgi-bin/wwwjdic.cgi";
 
-	protected static final Pattern PRE_START_PATTERN = Pattern
-			.compile("^<pre>.*$");
+    protected static final Pattern PRE_START_PATTERN = Pattern
+            .compile("^<pre>.*$");
 
-	protected static final Pattern PRE_END_PATTERN = Pattern
-			.compile("^</pre>.*$");
+    protected static final Pattern PRE_END_PATTERN = Pattern
+            .compile("^</pre>.*$");
 
-	public BackdoorTranslateTask(ResultListView resultListView,
-			SearchCriteria criteria) {
-		super(resultListView, criteria);
-	}
+    public BackdoorTranslateTask(ResultListView resultListView,
+            SearchCriteria criteria) {
+        super(resultListView, criteria);
+    }
 
-	@Override
-	protected abstract List<?> parseResult(String html);
+    @Override
+    protected List<T> parseResult(String html) {
+        List<T> result = new ArrayList<T>();
 
-	@Override
-	protected String query(SearchCriteria criteria) {
-		try {
-			String lookupUrl = String.format("%s?%s%s", BACKDOOR_URL,
-					generateBackdoorCode(criteria), URLEncoder.encode(criteria
-							.getQueryString(), "UTF-8"));
-			HttpGet get = new HttpGet(lookupUrl);
+        boolean isInPre = false;
+        String[] lines = html.split("\n");
+        for (String line : lines) {
+            if (StringUtils.isEmpty(line)) {
+                continue;
+            }
 
-			String responseStr = httpclient.execute(get, responseHandler,
-					localContext);
+            Matcher m = PRE_START_PATTERN.matcher(line);
+            if (m.matches()) {
+                isInPre = true;
+                continue;
+            }
 
-			return responseStr;
-		} catch (ClientProtocolException cpe) {
-			Log.e("WWWJDIC", "ClientProtocolException", cpe);
-			throw new RuntimeException(cpe);
-		} catch (IOException e) {
-			Log.e("WWWJDIC", "IOException", e);
-			throw new RuntimeException(e);
-		}
-	}
+            m = PRE_END_PATTERN.matcher(line);
+            if (m.matches()) {
+                break;
+            }
 
-	private String generateBackdoorCode(SearchCriteria criteria) {
-		StringBuffer buff = new StringBuffer();
-		buff.append(criteria.getDictionary());
-		// raw
-		buff.append("Z");
-		if (criteria.isKanjiLookup()) {
-			buff.append("M");
-		} else {
-			// unicode
-			buff.append("U");
-		}
-		if (criteria.isExactMatch()) {
-			buff.append("Q");
-		} else {
-			if (criteria.isKanjiLookup()) {
-				buff.append("J");
-			} else {
-				// English/Kanji
-				buff.append("E");
-				// for romanized Japanese
-				// buff.append("J");
-			}
-		}
+            if (isInPre) {
+                T entry = parseEntry(line);
+                result.add(entry);
+            }
+        }
 
-		return buff.toString();
-	}
+        return result;
+    }
+
+    protected abstract T parseEntry(String entryStr);
+
+    @Override
+    protected String query(SearchCriteria criteria) {
+        try {
+            String lookupUrl = String.format("%s?%s%s", BACKDOOR_URL,
+                    generateBackdoorCode(criteria), URLEncoder.encode(criteria
+                            .getQueryString(), "UTF-8"));
+            HttpGet get = new HttpGet(lookupUrl);
+
+            String responseStr = httpclient.execute(get, responseHandler,
+                    localContext);
+
+            return responseStr;
+        } catch (ClientProtocolException cpe) {
+            Log.e("WWWJDIC", "ClientProtocolException", cpe);
+            throw new RuntimeException(cpe);
+        } catch (IOException e) {
+            Log.e("WWWJDIC", "IOException", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String generateBackdoorCode(SearchCriteria criteria) {
+        StringBuffer buff = new StringBuffer();
+        buff.append(criteria.getDictionary());
+        // raw
+        buff.append("Z");
+        if (criteria.isKanjiLookup()) {
+            buff.append("M");
+        } else {
+            // unicode
+            buff.append("U");
+        }
+        if (criteria.isExactMatch()) {
+            buff.append("Q");
+        } else {
+            if (criteria.isKanjiLookup()) {
+                buff.append("J");
+            } else {
+                // English/Kanji
+                buff.append("E");
+                // for romanized Japanese
+                // buff.append("J");
+            }
+        }
+
+        return buff.toString();
+    }
 
 }
--- trunk/wwwjdic/src/org/nick/wwwjdic/DictionaryTranslateTask.java
+++ trunk/wwwjdic/src/org/nick/wwwjdic/DictionaryTranslateTask.java
@@ -1,39 +1,16 @@
 package org.nick.wwwjdic;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
 
-public class DictionaryTranslateTask extends BackdoorTranslateTask {
+public class DictionaryTranslateTask extends
+        BackdoorTranslateTask<DictionaryEntry> {
 
-	public DictionaryTranslateTask(DictionaryResultListView resultListView,
-			SearchCriteria criteria) {
-		super(resultListView, criteria);
-	}
+    public DictionaryTranslateTask(DictionaryResultListView resultListView,
+            SearchCriteria criteria) {
+        super(resultListView, criteria);
+    }
 
-	@Override
-	protected List<DictionaryEntry> parseResult(String html) {
-		List<DictionaryEntry> result = new ArrayList<DictionaryEntry>();
-
-		boolean isInPre = false;
-		String[] lines = html.split("\n");
-		for (String line : lines) {
-			Matcher m = PRE_START_PATTERN.matcher(line);
-			if (m.matches()) {
-				isInPre = true;
-			}
-
-			m = PRE_END_PATTERN.matcher(line);
-			if (m.matches()) {
-				break;
-			}
-
-			if (isInPre) {
-				DictionaryEntry entry = DictionaryEntry.parseEdict(line.trim());
-				result.add(entry);
-			}
-		}
-
-		return result;
-	}
+    @Override
+    protected DictionaryEntry parseEntry(String entryStr) {
+        return DictionaryEntry.parseEdict(entryStr.trim());
+    }
 }
