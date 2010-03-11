5e978566bea4ca6b9b4ec9fec8ced1d09a6c1f4e
nikolay.elenkov <nikolay.elenkov@74032484-283b-11df-b97d-a14e33d00e86>
2010-03-11 03:33:40 +0000

    properly parse new raw format

--- wwwjdic/src/org/nick/wwwjdic/KanjiTranslateTask.java
+++ wwwjdic/src/org/nick/wwwjdic/KanjiTranslateTask.java
@@ -1,39 +1,15 @@
 package org.nick.wwwjdic;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
 
-public class KanjiTranslateTask extends BackdoorTranslateTask {
+public class KanjiTranslateTask extends BackdoorTranslateTask<KanjiEntry> {
 
-	public KanjiTranslateTask(ResultListView resultListView,
-			SearchCriteria criteria) {
-		super(resultListView, criteria);
-	}
+    public KanjiTranslateTask(ResultListView resultListView,
+            SearchCriteria criteria) {
+        super(resultListView, criteria);
+    }
 
-	protected List<KanjiEntry> parseResult(String html) {
-		List<KanjiEntry> result = new ArrayList<KanjiEntry>();
-
-		boolean isInPre = false;
-		String[] lines = html.split("\n");
-		for (String line : lines) {
-			Matcher m = PRE_START_PATTERN.matcher(line);
-			if (m.matches()) {
-				isInPre = true;
-			}
-
-			m = PRE_END_PATTERN.matcher(line);
-			if (m.matches()) {
-				break;
-			}
-
-			if (isInPre) {
-				KanjiEntry entry = KanjiEntry.parseKanjidic(line.trim());
-
-				result.add(entry);
-			}
-		}
-
-		return result;
-	}
+    @Override
+    protected KanjiEntry parseEntry(String entryStr) {
+        return KanjiEntry.parseKanjidic(entryStr.trim());
+    }
 }
--- wwwjdic/src/org/nick/wwwjdic/StringUtils.java
+++ wwwjdic/src/org/nick/wwwjdic/StringUtils.java
@@ -35,4 +35,16 @@ public class StringUtils {
 
         return join(fieldsArr, separator, idx);
     }
+
+    public static boolean isEmpty(String str) {
+        if (str == null) {
+            return true;
+        }
+
+        if ("".equals(str)) {
+            return true;
+        }
+
+        return false;
+    }
 }
