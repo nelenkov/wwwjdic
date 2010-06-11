1171e45fb3765a78acd78b5d2e0043fc3e8b0041
nikolay.elenkov <nikolay.elenkov@74032484-283b-11df-b97d-a14e33d00e86>
2010-06-11 03:09:02 +0000

    animated stroke order diagrams

--- trunk/wwwjdic/src/org/nick/wwwjdic/sod/SodActivity.java
+++ trunk/wwwjdic/src/org/nick/wwwjdic/sod/SodActivity.java
@@ -1,11 +1,7 @@
 package org.nick.wwwjdic.sod;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -13,18 +9,16 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HttpContext;
+import org.nick.wwwjdic.Constants;
 import org.nick.wwwjdic.GzipStringResponseHandler;
 import org.nick.wwwjdic.R;
 import org.nick.wwwjdic.StringUtils;
 import org.nick.wwwjdic.WebServiceBackedActivity;
-import org.nick.wwwjdic.hkr.KanjiDrawView;
-import org.xmlpull.v1.XmlPullParser;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
-import android.util.Xml;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -50,24 +44,13 @@ public class SodActivity extends WebServiceBackedActivity implements
 
                 if (msg.arg1 == 1) {
                     final List<StrokePath> strokes = (List<StrokePath>) msg.obj;
+                    boolean animate = msg.arg2 == 1;
                     if (strokes != null) {
-                        Runnable r = new Runnable() {
-                            public void run() {
-                                List<StrokePath> toDraw = new ArrayList<StrokePath>();
-                                for (StrokePath s : strokes) {
-                                    toDraw.add(s);
-                                    sodActivity.drawSod(toDraw);
-                                    try {
-                                        Thread.sleep(100);
-                                    } catch (InterruptedException e) {
-                                        Log.e(TAG, e.getMessage(), e);
-                                    }
-                                }
-                            }
-                        };
-                        ExecutorService e = Executors.newSingleThreadExecutor();
-                        e.submit(r);
-                        // sodActivity.drawSod(strokes);
+                        if (animate) {
+                            sodActivity.animate(strokes);
+                        } else {
+                            sodActivity.drawSod(strokes);
+                        }
                     } else {
                         Toast t = Toast.makeText(sodActivity,
                                 String.format("No SOD data for '%s'",
@@ -81,6 +64,10 @@ public class SodActivity extends WebServiceBackedActivity implements
                     t.show();
                 }
                 break;
+            case REDRAW_SOD_MSG:
+                final List<StrokePath> strokes = (List<StrokePath>) msg.obj;
+                sodActivity.redraw(strokes);
+                break;
             default:
                 super.handleMessage(msg);
             }
@@ -91,18 +78,23 @@ public class SodActivity extends WebServiceBackedActivity implements
 
     private static final int STROKE_PATH_MSG = 1;
 
+    private static final int REDRAW_SOD_MSG = 2;
+
     private static final String STROKE_PATH_LOOKUP_URL = "http://wwwjdic-android.appspot.com/kanji/";
 
     private Button drawButton;
     private Button clearButton;
+    private Button animateButton;
 
-    private KanjiDrawView drawView;
+    private StrokeOrderView strokeOrderView;
 
     protected HttpContext localContext;
     private HttpClient httpClient;
 
     private String unicodeNumber;
     private String kanji;
+
+    private List<StrokePath> strokes;
 
     @Override
     protected void activityOnCreate(Bundle savedInstanceState) {
@@ -113,18 +105,54 @@ public class SodActivity extends WebServiceBackedActivity implements
 
         drawButton.setOnClickListener(this);
         clearButton.setOnClickListener(this);
+        animateButton.setOnClickListener(this);
 
         httpClient = createHttpClient();
-        unicodeNumber = getIntent().getExtras().getString("unicodeNumber");
-        kanji = getIntent().getExtras().getString("kanji");
+        unicodeNumber = getIntent().getExtras().getString(
+                Constants.KANJI_UNICODE_NUMBER);
+        kanji = getIntent().getExtras().getString(Constants.KANJI_GLYPH);
 
         setTitle(String.format("Stroke order diragram for '%s'", kanji));
+
+        drawSod();
     }
 
     public void drawSod(List<StrokePath> strokes) {
-        drawView.setStrokePaths(strokes);
-        drawView.invalidate();
+        this.strokes = new ArrayList<StrokePath>(strokes);
 
+        strokeOrderView.setStrokePaths(strokes);
+        strokeOrderView.setAnnotateStrokes(true);
+        strokeOrderView.invalidate();
+
+    }
+
+    public void redraw(List<StrokePath> strokes) {
+        strokeOrderView.setStrokePaths(strokes);
+        strokeOrderView.invalidate();
+    }
+
+    public void animate(final List<StrokePath> strokes) {
+        this.strokes = new ArrayList<StrokePath>(strokes);
+
+        strokeOrderView.setAnnotateStrokes(false);
+
+        Runnable animationTask = new Runnable() {
+            public void run() {
+                List<StrokePath> toDraw = new ArrayList<StrokePath>();
+                for (StrokePath s : strokes) {
+                    toDraw.add(s);
+                    Message msg = handler.obtainMessage(REDRAW_SOD_MSG);
+                    msg.obj = toDraw;
+                    handler.sendMessage(msg);
+                    try {
+                        Thread.sleep(700);
+                    } catch (InterruptedException e) {
+                        Log.e(TAG, e.getMessage(), e);
+                    }
+                }
+            }
+        };
+        getApp().getExecutorService().submit(animationTask);
     }
 
     @Override
@@ -135,8 +163,8 @@ public class SodActivity extends WebServiceBackedActivity implements
     private HttpClient createHttpClient() {
         HttpClient result = new DefaultHttpClient();
         HttpParams httpParams = result.getParams();
-        HttpConnectionParams.setConnectionTimeout(httpParams, 10 * 1000);
-        HttpConnectionParams.setSoTimeout(httpParams, 10 * 1000);
+        HttpConnectionParams.setConnectionTimeout(httpParams, 20 * 1000);
+        HttpConnectionParams.setSoTimeout(httpParams, 20 * 1000);
 
         return result;
     }
@@ -144,7 +172,8 @@ public class SodActivity extends WebServiceBackedActivity implements
     private void findViews() {
         drawButton = (Button) findViewById(R.id.draw_sod_button);
         clearButton = (Button) findViewById(R.id.clear_sod_button);
-        drawView = (KanjiDrawView) findViewById(R.id.sod_draw_view);
+        animateButton = (Button) findViewById(R.id.animate_button);
+        strokeOrderView = (StrokeOrderView) findViewById(R.id.sod_draw_view);
     }
 
     @Override
@@ -153,10 +182,12 @@ public class SodActivity extends WebServiceBackedActivity implements
         case R.id.draw_sod_button:
             drawSod();
             break;
+        case R.id.animate_button:
+            animate();
+            break;
         case R.id.clear_sod_button:
-            drawView.clear();
-            drawView.setStrokePaths(null);
-            drawView.invalidate();
+            strokeOrderView.clear();
+            strokeOrderView.invalidate();
             break;
         default:
             // do nothing
@@ -166,12 +197,15 @@ public class SodActivity extends WebServiceBackedActivity implements
     class GetStrokePathTask implements Runnable {
 
         private String unicodeNumber;
+        private boolean animate;
+
         private Handler handler;
         private HttpClient httpClient;
 
-        public GetStrokePathTask(String unicodeNumber, HttpClient httpClient,
-                Handler handler) {
+        public GetStrokePathTask(String unicodeNumber, boolean animate,
+                HttpClient httpClient, Handler handler) {
             this.unicodeNumber = unicodeNumber;
+            this.animate = animate;
             this.handler = handler;
             this.httpClient = httpClient;
         }
@@ -185,10 +219,12 @@ public class SodActivity extends WebServiceBackedActivity implements
             try {
                 String responseStr = httpClient.execute(get,
                         new GzipStringResponseHandler(), localContext);
+                Log.d(TAG, "got SOD response: " + responseStr);
 
                 List<StrokePath> strokes = parseWsReply(responseStr);
                 Message msg = handler.obtainMessage(STROKE_PATH_MSG, strokes);
                 msg.arg1 = 1;
+                msg.arg2 = animate ? 1 : 0;
                 handler.sendMessage(msg);
             } catch (Exception e) {
                 Message msg = handler.obtainMessage(STROKE_PATH_MSG);
@@ -200,9 +236,23 @@ public class SodActivity extends WebServiceBackedActivity implements
     }
 
     private void drawSod() {
-        Runnable getStrokesTask = new GetStrokePathTask(unicodeNumber,
-                httpClient, handler);
-        submitWsTask(getStrokesTask, "Getting SOD info...");
+        if (strokes == null) {
+            Runnable getStrokesTask = new GetStrokePathTask(unicodeNumber,
+                    false, httpClient, handler);
+            submitWsTask(getStrokesTask, "Getting SOD info...");
+        } else {
+            drawSod(strokes);
+        }
+    }
+
+    private void animate() {
+        if (strokes == null) {
+            Runnable getStrokesTask = new GetStrokePathTask(unicodeNumber,
+                    true, httpClient, handler);
+            submitWsTask(getStrokesTask, "Getting SOD info...");
+        } else {
+            animate(strokes);
+        }
     }
 
     private List<StrokePath> parseWsReply(String reply) {
@@ -226,46 +276,6 @@ public class SodActivity extends WebServiceBackedActivity implements
         }
 
         return result;
-    }
-
-    private List<StrokePath> parseKangiVgXml() {
-        // File f = new File("/sdcard/wwwjdic/onekanji.xml");
-        File f = new File("/sdcard/wwwjdic/k2.xml");
-
-        List<StrokePath> strokes = new ArrayList<StrokePath>();
-        XmlPullParser parser = Xml.newPullParser();
-
-        try {
-            // auto-detect the encoding from the stream
-            parser.setInput(new FileInputStream(f), null);
-            int eventType = parser.getEventType();
-            boolean done = false;
-
-            while (eventType != XmlPullParser.END_DOCUMENT && !done) {
-                String name = null;
-                switch (eventType) {
-                case XmlPullParser.START_DOCUMENT:
-                    break;
-                case XmlPullParser.START_TAG:
-                    name = parser.getName();
-                    if (name.equalsIgnoreCase("stroke")) {
-                        String path = parser.getAttributeValue(null, "path");
-                        StrokePath strokePath = StrokePath.parsePath(path);
-                        strokes.add(strokePath);
-                    }
-                    break;
-                case XmlPullParser.END_TAG:
-                    name = parser.getName();
-
-                    break;
-                }
-                eventType = parser.next();
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        return strokes;
     }
 
     public String getUnicodeNumber() {
