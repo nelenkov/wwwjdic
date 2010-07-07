17c07bc8dee380d9757bfe62011c585167b85e2a
nikolay.elenkov <nikolay.elenkov@74032484-283b-11df-b97d-a14e33d00e86>
2010-07-07 03:27:49 +0000

    import/export favorites

--- wwwjdic/src/org/nick/wwwjdic/history/Favorites.java
+++ wwwjdic/src/org/nick/wwwjdic/history/Favorites.java
@@ -1,5 +1,10 @@
 package org.nick.wwwjdic.history;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.nick.wwwjdic.Constants;
 import org.nick.wwwjdic.DictionaryEntryDetail;
 import org.nick.wwwjdic.KanjiEntryDetail;
@@ -9,6 +14,12 @@ import org.nick.wwwjdic.history.FavoritesItem.FavoriteStatusChangedListener;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
 
 public class Favorites extends HistoryBase implements
         FavoriteStatusChangedListener {
@@ -85,13 +96,81 @@ public class Favorites extends HistoryBase implements
 
     @Override
     protected void exportItems() {
-        // TODO Auto-generated method stub
+        CSVWriter writer = null;
+
+        try {
+            Cursor c = db.getFavorites();
+            File extStorage = Environment.getExternalStorageDirectory();
+            String exportFile = extStorage.getAbsolutePath() + "/favorites.csv";
+            writer = new CSVWriter(new FileWriter(exportFile));
+
+            while (c.moveToNext()) {
+                WwwjdicEntry entry = HistoryDbHelper.createWwwjdicEntry(c);
+                long time = c.getLong(c.getColumnIndex("time"));
+                String[] entryStr = FavoritesEntryParser.toStringArray(entry,
+                        time);
+                writer.writeNext(entryStr);
+
+            }
+
+            String message = getResources().getString(
+                    R.string.favorites_exported);
+            Toast t = Toast.makeText(this, String.format(message, exportFile),
+                    Toast.LENGTH_SHORT);
+            t.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    Log.w(TAG, "error closing CSV writer", e);
+                }
+            }
+        }
     }
 
     @Override
     protected void importItems() {
-        // TODO Auto-generated method stub
+        CSVReader reader = null;
 
+        SQLiteDatabase s = db.getWritableDatabase();
+        s.beginTransaction();
+        try {
+            File extStorage = Environment.getExternalStorageDirectory();
+            String importFile = extStorage.getAbsolutePath() + "/favorites.csv";
+            reader = new CSVReader(new FileReader(importFile));
+
+            String[] record = null;
+            while ((record = reader.readNext()) != null) {
+                WwwjdicEntry entry = FavoritesEntryParser
+                        .fromStringArray(record);
+                long time = Long.parseLong(record[3]);
+                db.addFavorite(entry, time);
+
+            }
+            s.setTransactionSuccessful();
+
+            refresh();
+            String message = getResources().getString(
+                    R.string.favorites_imported);
+            Toast t = Toast.makeText(this, String.format(message, importFile),
+                    Toast.LENGTH_SHORT);
+            t.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    Log.w(TAG, "error closing CSV reader", e);
+                }
+
+            }
+            s.endTransaction();
+        }
     }
 
 }
