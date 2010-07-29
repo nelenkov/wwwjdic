3314a6a0e72755b64543cf3bf5933d80424df524
nikolay.elenkov <nikolay.elenkov@74032484-283b-11df-b97d-a14e33d00e86>
2010-07-29 01:36:54 +0000

    use 'auto' mode for OCR (seems to give better resutls, see (Issue 12))

--- trunk/wwwjdic/src/org/nick/wwwjdic/ocr/WeOcrClient.java
+++ trunk/wwwjdic/src/org/nick/wwwjdic/ocr/WeOcrClient.java
@@ -19,8 +19,6 @@ public class WeOcrClient extends EntityBasedHttpClient {
 
     private static final String TAG = WeOcrClient.class.getSimpleName();
 
-    private static final String ECLASS_CHAR = "character";
-
     public WeOcrClient(String endpoint, int timeout) {
         super(endpoint, timeout);
     }
@@ -56,7 +54,8 @@ public class WeOcrClient extends EntityBasedHttpClient {
     public String[] sendCharacterOcrRequest(Bitmap img, int numCandidates)
             throws IOException {
         Log.i(TAG, "Sending OCR request to " + url);
-        HttpPost post = createPost(img, ECLASS_CHAR, numCandidates);
+        HttpPost post = createPost(img, OcrFormEntity.ECLASS_CHAR,
+                numCandidates);
 
         BufferedReader reader = null;
         try {
