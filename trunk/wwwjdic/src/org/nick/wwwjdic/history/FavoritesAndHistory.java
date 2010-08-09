b9885cafc6398d4b734301af091072abe51741d6
nikolay.elenkov <nikolay.elenkov@74032484-283b-11df-b97d-a14e33d00e86>
2010-08-09 07:02:44 +0000

    add favorites/history summary to each tab

--- trunk/wwwjdic/src/org/nick/wwwjdic/history/FavoritesAndHistory.java
+++ trunk/wwwjdic/src/org/nick/wwwjdic/history/FavoritesAndHistory.java
@@ -1,5 +1,6 @@
 package org.nick.wwwjdic.history;
 
+import org.nick.wwwjdic.Constants;
 import org.nick.wwwjdic.R;
 
 import android.app.TabActivity;
@@ -16,23 +17,38 @@ public class FavoritesAndHistory extends TabActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favorites_history);
 
-        setupTabs();
+        Intent intent = getIntent();
+        int filterType = intent.getIntExtra(Constants.FILTER_TYPE, 0);
+        int tabIdx = intent.getIntExtra(
+                Constants.FAVORITES_HISTORY_SELECTED_TAB_IDX, 0);
+
+        setupTabs(tabIdx, filterType);
     }
 
-    private void setupTabs() {
+    private void setupTabs(int tabIdx, int filterType) {
         TabHost tabHost = getTabHost();
 
+        Intent favoritesIntent = new Intent(this, Favorites.class);
+        if (tabIdx == 0) {
+            favoritesIntent.putExtra(Constants.FILTER_TYPE, filterType);
+        }
         tabHost.addTab(tabHost.newTabSpec("favorites").setIndicator(
                 getResources().getString(R.string.favorites),
                 getResources().getDrawable(R.drawable.ic_tab_favorites))
-                .setContent(new Intent(this, Favorites.class)));
+                .setContent(favoritesIntent));
+
+        Intent historyIntent = new Intent(this, SearchHistory.class);
+        if (tabIdx == 1) {
+            historyIntent.putExtra(Constants.FILTER_TYPE, filterType);
+        }
         tabHost.addTab(tabHost.newTabSpec("history").setIndicator(
                 getResources().getString(R.string.search_history),
                 getResources().getDrawable(R.drawable.ic_tab_history))
-                .setContent(new Intent(this, SearchHistory.class)));
+                .setContent(historyIntent));
 
         tabHost.setOnTabChangedListener(this);
-        tabHost.setCurrentTab(0);
+
+        tabHost.setCurrentTab(tabIdx);
     }
 
     @Override
