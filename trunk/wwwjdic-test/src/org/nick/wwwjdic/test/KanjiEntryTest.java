5b00e1bf62e3c2db0b8bb720a898092932d153c8
nikolay.elenkov <nikolay.elenkov@74032484-283b-11df-b97d-a14e33d00e86>
2010-09-02 03:40:11 +0000

    more tests

--- trunk/wwwjdic-test/src/org/nick/wwwjdic/test/KanjiEntryTest.java
+++ trunk/wwwjdic-test/src/org/nick/wwwjdic/test/KanjiEntryTest.java
@@ -15,6 +15,8 @@ import org.nick.wwwjdic.KanjiEntry;
 
 public class KanjiEntryTest {
 
+    private static final String KANJIDIC_PATH = "C:/home/nick/android/wwwjdic/wwwjdic-test/dict/kanjidic";
+
     @Test
     public void testParse() {
         String kanjidicStr = "違 3063 U9055 B162 G8 S13 S12 F344 J2 N4720 V6099 H3151 DK2014 L1644 K496 DO883 MN39067P MP11.0144 E1006 IN814 DF965 DC274 DJ385 DG713 DM1659 P3-3-10 I2q10.5 Q3430.4 DR1655 ZSP3-2-10 Ywei2 Wwi イ ちが.う ちが.い ちが.える -ちが.える たが.う たが.える {difference} {differ} ";
@@ -75,7 +77,7 @@ public class KanjiEntryTest {
     }
 
     @Test
-    public void testParseNanori2() {
+    public void testParseNanoriNoKunyomi() {
         String kanjidicStr = "絢 303C U7d62 B120 G9 S12 F2315 J1 N3530 V4481 H1347 DK911 L2664 O2150 MN27427 MP8.1051 IN2194 P1-6-6 I6a6.14 Q2792.0 Yxuan4 Whyeon ケン T1 じゅん あや {kimono design}";
         KanjiEntry entry = KanjiEntry.parseKanjidic(kanjidicStr);
         assertNotNull(entry);
@@ -96,14 +98,48 @@ public class KanjiEntryTest {
         assertEquals("ケン じゅん あや", entry.getReading());
         assertEquals("ケン", entry.getOnyomi());
         assertEquals("", entry.getKunyomi());
+        assertEquals("じゅん あや", entry.getNanori());
         assertEquals(1, entry.getMeanings().size());
         assertEquals("kimono design", entry.getMeanings().get(0));
     }
 
     @Test
+    public void testParseNanoriLong() {
+        String kanjidicStr = "生 4038 U751f B100 G1 S5 F29 J4 N2991 V3715 H3497 DK2179 L1555 K29 O214 DO67 MN21670 MP7.1027 E42 IN44 DS34 DF71 DH44 DT43 DC9 DJ49 DB2.4 DG1327 DM1569 P4-5-2 I0a5.29 Q2510.0 DR2472 Ysheng1 Wsaeng セイ ショウ い.きる い.かす い.ける う.まれる うま.れる う.まれ うまれ う.む お.う は.える は.やす き なま なま- な.る な.す む.す -う T1 あさ いき いく いけ うぶ うまい え おい ぎゅう くるみ ごせ さ じょう すぎ そ そう ちる なば にう にゅう ふ み もう よい りゅう {life} {genuine} {birth}";
+        KanjiEntry entry = KanjiEntry.parseKanjidic(kanjidicStr);
+        assertNotNull(entry);
+
+        assertEquals("生", entry.getKanji());
+        assertNull(entry.getClassicalRadicalNumber());
+        assertEquals(29, entry.getFrequncyeRank().intValue());
+        assertEquals(1, entry.getGrade().intValue());
+        assertEquals("4038", entry.getJisCode());
+        assertEquals(4, entry.getJlptLevel().intValue());
+        assertEquals("saeng", entry.getKoreanReading());
+        assertEquals("sheng1", entry.getPinyin());
+        assertEquals(100, entry.getRadicalNumber());
+        assertEquals("4-5-2", entry.getSkipCode());
+        assertEquals(5, entry.getStrokeCount());
+        assertEquals("751f", entry.getUnicodeNumber());
+        assertEquals(
+                "セイ ショウ い.きる い.かす い.ける う.まれる うま.れる う.まれ うまれ う.む お.う は.える は.やす き なま なま- な.る な.す む.す -う あさ いき いく いけ うぶ うまい え おい ぎゅう くるみ ごせ さ じょう すぎ そ そう ちる なば にう にゅう ふ み もう よい りゅう",
+                entry.getReading());
+        assertEquals("セイ ショウ", entry.getOnyomi());
+        assertEquals(
+                "い.きる い.かす い.ける う.まれる うま.れる う.まれ うまれ う.む お.う は.える は.やす き なま なま- な.る な.す む.す -う",
+                entry.getKunyomi());
+        assertEquals(
+                "あさ いき いく いけ うぶ うまい え おい ぎゅう くるみ ごせ さ じょう すぎ そ そう ちる なば にう にゅう ふ み もう よい りゅう",
+                entry.getNanori());
+        assertEquals(3, entry.getMeanings().size());
+        assertEquals("life", entry.getMeanings().get(0));
+        assertEquals("genuine", entry.getMeanings().get(1));
+        assertEquals("birth", entry.getMeanings().get(2));
+    }
+
+    @Test
     public void testKanjidic() throws Exception {
-        FileInputStream fis = new FileInputStream(
-                "/home/nick/android/wwwjdic/dict/kanjidic");
+        FileInputStream fis = new FileInputStream(KANJIDIC_PATH);
         List<String> lines = new ArrayList<String>();
         BufferedReader r = new BufferedReader(new InputStreamReader(fis,
                 "EUC-JP"));
