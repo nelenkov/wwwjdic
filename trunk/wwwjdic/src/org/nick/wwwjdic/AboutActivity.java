01323f023f50c4d2701a63f8a8fb19da77a5d967
nikolay.elenkov <nikolay.elenkov@74032484-283b-11df-b97d-a14e33d00e86>
2010-09-03 04:19:24 +0000

    added link to donate version

--- trunk/wwwjdic/src/org/nick/wwwjdic/AboutActivity.java
+++ trunk/wwwjdic/src/org/nick/wwwjdic/AboutActivity.java
@@ -1,10 +1,17 @@
 package org.nick.wwwjdic;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class AboutActivity extends Activity {
+public class AboutActivity extends Activity implements OnClickListener {
+
+    private static final String DONATE_VERSION_PACKAGE = "org.nick.wwwjdic.donate";
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -14,5 +21,15 @@ public class AboutActivity extends Activity {
 
         TextView versionText = (TextView) findViewById(R.id.versionText);
         versionText.setText("version " + WwwjdicApplication.getVersion());
+
+        Button buyDonateButton = (Button) findViewById(R.id.buy_donate);
+        buyDonateButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri
+                .parse("market://details?id=" + DONATE_VERSION_PACKAGE));
+        startActivity(intent);
     }
 }
