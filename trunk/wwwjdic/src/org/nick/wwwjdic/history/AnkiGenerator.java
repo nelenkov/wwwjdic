5eb17147a3f68cf14e82ba9f32ef4d01b37afdc3
nikolay.elenkov <nikolay.elenkov@74032484-283b-11df-b97d-a14e33d00e86>
2010-11-29 10:03:07 +0000

    Anki export for kanji favorites

--- trunk/wwwjdic/src/org/nick/wwwjdic/history/AnkiGenerator.java
+++ trunk/wwwjdic/src/org/nick/wwwjdic/history/AnkiGenerator.java
@@ -3,7 +3,14 @@ package org.nick.wwwjdic.history;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.List;
+import java.util.Random;
 
+import org.nick.wwwjdic.KanjiEntry;
+
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.database.sqlite.SQLiteDatabase;
@@ -14,28 +21,75 @@ public class AnkiGenerator {
 
     private static final String TAG = AnkiGenerator.class.getSimpleName();
 
+    private static final long MODEL_ID = 4998196432932412600L;
+    private static final long FWD_CARD_MODEL_ID = 8257311625381387448L;
+
+    private static final long KANJI_FIELD_ID = -4886934269285393224L;
+    private static final long ONYOMI_FIELD_ID = 4393069213469613240L;
+    private static final long KUNYOMI_FIELD_ID = 760313581623303912L;
+    private static final long NANORI_FIELD_ID = -7227726355099557880L;
+    private static final long MEANING_FIELD_ID = -3128184056797208296L;
+
+    private static final int KANJI_FIELD_ORD = 0;
+    private static final int ONYOMI_FIELD_ORD = 1;
+    private static final int KUNYOMI_FIELD_ORD = 2;
+    private static final int NANORI_FIELD_ORD = 3;
+    private static final int MEANING_FIELD_ORD = 4;
+
+    private static final String QA_TEMPLATE = "<span class=\"fm3cf7512c968f9cb8\">%s</span><br>";
+
     private Context context;
+    private Random random;
 
     public AnkiGenerator(Context context) {
         this.context = context;
+        try {
+            this.random = SecureRandom.getInstance("SHA1PRNG");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void createAnkiFile(String path) {
+    public void createAnkiFile(String path, List<KanjiEntry> kanjis) {
         SQLiteDatabase db = null;
 
         try {
             db = SQLiteDatabase.openDatabase(path, null,
                     SQLiteDatabase.CREATE_IF_NECESSARY);
-            String schema = readSchema();
-            String[] statements = schema.split(";");
             db.beginTransaction();
+
+            String schema = readSchema("anki-create.sql");
+            String[] statements = schema.split(";");
             for (String s : statements) {
+                if (s == null) {
+                    continue;
+                }
+                s = s.trim();
                 Log.d(TAG, "SQL: " + s);
-                if (TextUtils.isEmpty(s) || "\n".equals(s)) {
+                if (TextUtils.isEmpty(s) || s.startsWith("--")) {
                     continue;
                 }
                 db.execSQL(s.trim());
             }
+
+            schema = readSchema("anki-model.sql");
+            statements = schema.split(";");
+            for (String s : statements) {
+                if (s == null) {
+                    continue;
+                }
+                s = s.trim();
+                Log.d(TAG, "SQL: " + s);
+                if (TextUtils.isEmpty(s) || s.startsWith("--")) {
+                    continue;
+                }
+                db.execSQL(s.trim());
+            }
+
+            for (KanjiEntry k : kanjis) {
+                addKanji(db, k);
+            }
+
             db.setTransactionSuccessful();
 
         } finally {
@@ -46,12 +100,129 @@ public class AnkiGenerator {
         }
     }
 
-    private String readSchema() {
+    private void addKanji(SQLiteDatabase db, KanjiEntry k) {
+        ContentValues fact = new ContentValues();
+        long factId = generateId();
+        fact.put("id", factId);
+        fact.put("modelId", MODEL_ID);
+        long now = System.currentTimeMillis();
+        fact.put("created", now);
+        fact.put("modified", now);
+        fact.put("tags", "");
+        fact.put("spaceUntil", 0.0);
+
+        db.insert("facts", null, fact);
+
+        ContentValues card = new ContentValues();
+        long cardId = generateId();
+        card.put("id", cardId);
+        card.put("factId", factId);
+        card.put("cardModelId", FWD_CARD_MODEL_ID);
+        card.put("created", now);
+        card.put("modified", now);
+        card.put("tags", "");
+        card.put("ordinal", 0);
+        card.put("question", generateQuestion(k));
+        card.put("answer", generateAnswer(k));
+        card.put("priority", 2);
+        card.put("interval", 0.0);
+        card.put("lastInterval", 0.0);
+        card.put("due", now);
+        card.put("lastDue", 0);
+        card.put("factor", 2.5);
+        card.put("lastFactor", 2.5);
+        card.put("firstAnswered", 0);
+        card.put("reps", 0);
+        card.put("successive", 0);
+        card.put("averageTime", 0);
+        card.put("reviewTime", 0);
+        card.put("youngEase0", 0);
+        card.put("youngEase1", 0);
+        card.put("youngEase2", 0);
+        card.put("youngEase3", 0);
+        card.put("youngEase4", 0);
+        card.put("matureEase0", 0);
+        card.put("matureEase1", 0);
+        card.put("matureEase2", 0);
+        card.put("matureEase3", 0);
+        card.put("matureEase4", 0);
+        card.put("yesCount", 0);
+        card.put("noCount", 0);
+        card.put("spaceUntil", 0);
+        card.put("relativeDelay", 0);
+
+        card.put("isDue", true);
+        card.put("type", 2);
+        card.put("combinedDue", now);
+        db.insert("cards", null, card);
+
+        insertField(db, k.getKanji(), factId, KANJI_FIELD_ID, KANJI_FIELD_ORD);
+        if (k.getOnyomi() != null) {
+            insertField(db, k.getOnyomi(), factId, ONYOMI_FIELD_ID,
+                    ONYOMI_FIELD_ORD);
+        }
+        if (k.getKunyomi() != null) {
+            insertField(db, k.getKunyomi(), factId, KUNYOMI_FIELD_ID,
+                    KUNYOMI_FIELD_ORD);
+        }
+        if (k.getNanori() != null) {
+            insertField(db, k.getNanori(), factId, NANORI_FIELD_ID,
+                    NANORI_FIELD_ORD);
+        }
+        if (k.getMeaningsAsString() != null) {
+            insertField(db, k.getMeaningsAsString(), factId, MEANING_FIELD_ID,
+                    MEANING_FIELD_ORD);
+        }
+    }
+
+    private void insertField(SQLiteDatabase db, String value, long factId,
+            long fieldModelId, int ordinal) {
+        ContentValues field = new ContentValues();
+        field.put("id", generateId());
+        field.put("factId", factId);
+        field.put("fieldModelId", fieldModelId);
+        field.put("ordinal", ordinal);
+        field.put("value", value);
+        db.insert("fields", null, field);
+    }
+
+    private String generateAnswer(KanjiEntry k) {
+        StringBuffer buff = new StringBuffer();
+        if (k.getOnyomi() != null) {
+            buff.append(String.format(QA_TEMPLATE, k.getOnyomi()));
+        }
+        if (k.getKunyomi() != null) {
+            buff.append(String.format(QA_TEMPLATE, k.getKunyomi()));
+        }
+        if (k.getNanori() != null) {
+            buff.append(String.format(QA_TEMPLATE, k.getNanori()));
+        }
+        if (k.getMeaningsAsString() != null) {
+            buff.append(String.format(QA_TEMPLATE, k.getMeaningsAsString()));
+        }
+
+        return buff.toString();
+    }
+
+    private String generateQuestion(KanjiEntry k) {
+        return String.format("<span class=\"fmbc2e1f2c968f9cb8\">%s</span>", k
+                .getKanji());
+    }
+
+    private long generateId() {
+        long time = System.currentTimeMillis();
+        int rand = random.nextInt(2 ^ 23);
+        long id = rand << 41 | time;
+
+        return id;
+    }
+
+    private String readSchema(String name) {
         AssetManager assetManager = context.getAssets();
 
         InputStream in = null;
         try {
-            in = assetManager.open("anki-create.sql");
+            in = assetManager.open(name);
 
             return readTextFile(in);
         } catch (IOException e) {
