0f4f49db7b0859ac653487b3714ba28dc5395e5b
nikolay.elenkov <nikolay.elenkov@74032484-283b-11df-b97d-a14e33d00e86>
2010-11-30 04:39:26 +0000

    cross references in dictionary details, see (Issue 3)

--- wwwjdic/src/org/nick/wwwjdic/DetailActivity.java
+++ wwwjdic/src/org/nick/wwwjdic/DetailActivity.java
@@ -1,19 +1,29 @@
 package org.nick.wwwjdic;
 
+import java.util.regex.Pattern;
+
 import org.nick.wwwjdic.history.HistoryDbHelper;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.text.ClipboardManager;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
 import android.view.View;
 import android.view.View.OnLongClickListener;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class DetailActivity extends Activity implements
         OnCheckedChangeListener, OnLongClickListener {
 
+    protected static final Pattern CROSS_REF_PATTERN = Pattern
+            .compile("^.*\\(See (\\S+)\\).*$");
     protected HistoryDbHelper db;
     protected WwwjdicEntry wwwjdicEntry;
     protected boolean isFavorite;
@@ -51,5 +61,24 @@ public class DetailActivity extends Activity implements
         t.show();
 
         return false;
+    }
+
+    protected WwwjdicApplication getApp() {
+        return (WwwjdicApplication) getApplication();
+    }
+
+    protected void makeClickable(TextView textView, int start, int end,
+            Intent intent) {
+        SpannableString str = SpannableString.valueOf(textView.getText());
+        str.setSpan(new IntentSpan(this, intent), start, end,
+                Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+        textView.setText(str);
+        // textView.setLinkTextColor(Color.WHITE);
+        MovementMethod m = textView.getMovementMethod();
+        if ((m == null) || !(m instanceof LinkMovementMethod)) {
+            if (textView.getLinksClickable()) {
+                textView.setMovementMethod(LinkMovementMethod.getInstance());
+            }
+        }
     }
 }
