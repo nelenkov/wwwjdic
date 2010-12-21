b25bb7114b35e8a1291daed68665b01c88c49962
nikolay.elenkov <nikolay.elenkov@74032484-283b-11df-b97d-a14e33d00e86>
2010-12-21 07:30:54 +0000

    use Unicode cp to get kanji of the day; fixed Level 1 kanji generation bug

--- wwwjdic/src/org/nick/wwwjdic/widgets/RandomJisGenerator.java
+++ wwwjdic/src/org/nick/wwwjdic/widgets/RandomJisGenerator.java
@@ -1,5 +1,6 @@
 package org.nick.wwwjdic.widgets;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Random;
 
 public class RandomJisGenerator {
@@ -9,28 +10,38 @@ public class RandomJisGenerator {
     // * 94x94 grid
     // * kanji: lines 16-83
     // * line 84 -- 6 kanji
-    // * raw JIS: the 16-bit code resulting from adding 33 to each
+    // * raw JIS: the 16-bit code resulting from adding 0x20 to each
     // JIS coordinate and concatenating the two resulting bytes,
     // the vertical coordinate becoming the high byte. (coords are zero based)
 
+    // Cf.
+    // ftp://ftp.unicode.org/Public/MAPPINGS/OBSOLETE/EASTASIA/JIS/JIS0208.TXT
+    // To change hex to EUC form, add 0x8080
+    // To change hex to kuten form, first subtract 0x2020. Then
+    // the high and low bytes correspond to the ku and ten of
+    // the kuten form. For example, 0x2121 -> 0x0101 -> 0101;
+    // 0x7426 -> 0x5406 -> 8406
+
     private static final int JIS_GRID_SIZE = 94;
     // zero based
-    private static final int KANJI_START_LINE = 15;
-    private static final int KANJI_END_LINE = 83;
+    private static final int KANJI_START_LINE = 16;
+    private static final int KANJI_END_LINE = 84;
     private static final int NUM_KANJI_LAST_LINE = 6;
 
-    private static final int LEVEL1_START_LINE = 16;
+    // zero based
+    private static final int LEVEL1_START_LINE = 14;
     private static final int LEVEL1_END_LINE = 47;
+    private static final int LEVEL1_END_LINE_NUM_KANJI = 51;
 
-    private static final int OFFSET = 33;
+    private static final int OFFSET = 0x20;
 
     private Random random = new Random();
 
-    public String generate() {
-        return generate(false);
+    public String generateRawJis() {
+        return generateRawJis(false);
     }
 
-    public String generate(boolean limitToLevelOne) {
+    public String generateRawJis(boolean limitToLevelOne) {
         int startLine = KANJI_START_LINE;
         int endLine = KANJI_END_LINE;
 
@@ -38,17 +49,44 @@ public class RandomJisGenerator {
             startLine = LEVEL1_START_LINE;
             endLine = LEVEL1_END_LINE;
         }
+
         int line = random.nextInt(endLine - startLine + 1) + startLine;
 
         int column = 0;
-        if (line != KANJI_END_LINE) {
-            column = random.nextInt(JIS_GRID_SIZE);
-        } else {
+        if (limitToLevelOne && line == LEVEL1_END_LINE) {
+            column = random.nextInt(LEVEL1_END_LINE_NUM_KANJI) + 1;
+        } else if (!limitToLevelOne && line == KANJI_END_LINE) {
             column = random.nextInt(NUM_KANJI_LAST_LINE) + 1;
+        } else {
+            column = random.nextInt(JIS_GRID_SIZE);
         }
+
         int x = line + OFFSET;
         int y = column + OFFSET;
 
         return Integer.toString(x, 16) + Integer.toString(y, 16);
+    }
+
+    public String generateAsUnicodeCp(boolean limitToLevelOne) {
+        return rawJisToUnicodeCp(generateRawJis(limitToLevelOne));
+    }
+
+    private static String rawJisToUnicodeCp(String rawJis) {
+        if (rawJis.length() != 4) {
+            throw new IllegalArgumentException("Invalid raw JIS code");
+        }
+        byte high = Byte.parseByte(rawJis.substring(0, 2), 16);
+        byte low = Byte.parseByte(rawJis.substring(2), 16);
+
+        byte[] eucBytes = { (byte) (high + 0x80), (byte) (low + 0x80) };
+        try {
+            String kanji = new String(eucBytes, "EUC-JP");
+            int unicodeCp = (int) kanji.toCharArray()[0];
+
+            return Integer.toString(unicodeCp, 16);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
