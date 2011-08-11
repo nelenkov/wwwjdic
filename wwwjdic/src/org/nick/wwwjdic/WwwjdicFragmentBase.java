5db9535f9061ed3e20d85d63e1cfc05cea56990c
nikolay.elenkov@gmail.com <nikolay.elenkov@gmail.com@74032484-283b-11df-b97d-a14e33d00e86>
2011-08-11 05:41:21 +0000

    use fragments and action bar for main activity

---
+++ wwwjdic/src/org/nick/wwwjdic/WwwjdicFragmentBase.java
@@ -0,0 +1,21 @@
+package org.nick.wwwjdic;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class WwwjdicFragmentBase extends Fragment {
+
+    protected static final int NUM_RECENT_HISTORY_ENTRIES = 5;
+
+    protected boolean inputTextFromBundle;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected WwwjdicApplication getApp() {
+        return (WwwjdicApplication) getActivity().getApplication();
+    }
+
+}
