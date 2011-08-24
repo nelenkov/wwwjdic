e18c0665de74ffd7016e91eaa38cbf8713cfd287
nikolay.elenkov@gmail.com <nikolay.elenkov@gmail.com@74032484-283b-11df-b97d-a14e33d00e86>
2011-08-24 09:32:29 +0000

    don't try to nest fragments

--- wwwjdic/src/org/nick/wwwjdic/Wwwjdic.java
+++ wwwjdic/src/org/nick/wwwjdic/Wwwjdic.java
@@ -5,6 +5,9 @@ import static org.nick.wwwjdic.Constants.EXAMPLE_SEARRCH_TAB_IDX;
 import static org.nick.wwwjdic.Constants.KANJI_TAB_IDX;
 import static org.nick.wwwjdic.Constants.SELECTED_TAB_IDX;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.nick.wwwjdic.history.FavoritesAndHistory;
 import org.nick.wwwjdic.hkr.RecognizeKanjiActivity;
 import org.nick.wwwjdic.ocr.OcrActivity;
@@ -16,14 +19,25 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.ActionBar;
+import android.support.v4.app.ActionBar.Tab;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.Menu;
 import android.support.v4.view.MenuItem;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuInflater;
+import android.view.View;
 
 public class Wwwjdic extends FragmentActivity {
+
+    private static final String TAG = Wwwjdic.class.getSimpleName();
 
     private static final int WHATS_NEW_DIALOG_ID = 1;
     private static final int DONATION_THANKS_DIALOG_ID = 2;
@@ -32,8 +46,164 @@ public class Wwwjdic extends FragmentActivity {
 
     private static final boolean IS_HONEYCOMB = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
 
+    class WwwjdicTabsPagerAdapter extends PagerAdapter implements
+            ViewPager.OnPageChangeListener, ActionBar.TabListener {
+
+        private FragmentActivity activity;
+        private FragmentManager fragmentManager;
+        private FragmentTransaction currentTransaction = null;
+
+        private final ActionBar actionBar;
+        private final ViewPager viewPager;
+        private final List<Fragment> tabs = new ArrayList<Fragment>();
+
+        private final List<Integer> tabLayouts = new ArrayList<Integer>();
+        private final List<View> tabViews = new ArrayList<View>();
+
+        public WwwjdicTabsPagerAdapter(FragmentActivity activity,
+                ActionBar actionBar, ViewPager pager) {
+            this.activity = activity;
+            this.fragmentManager = activity.getSupportFragmentManager();
+            this.actionBar = actionBar;
+            this.viewPager = pager;
+            this.viewPager.setAdapter(this);
+            this.viewPager.setOnPageChangeListener(this);
+        }
+
+        public void addTab(ActionBar.Tab tab, int layoutId) {
+            tabLayouts.add(layoutId);
+            actionBar.addTab(tab.setTabListener(this));
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public void onTabReselected(Tab tab, FragmentTransaction ft) {
+        }
+
+        @Override
+        public void onTabSelected(Tab tab, FragmentTransaction ft) {
+            viewPager.setCurrentItem(tab.getPosition());
+        }
+
+        @Override
+        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+        }
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset,
+                int positionOffsetPixels) {
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            actionBar.setSelectedNavigationItem(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+        }
+
+        @Override
+        public int getCount() {
+            return tabLayouts.size();
+        }
+
+        @Override
+        public void startUpdate(View container) {
+        }
+
+        @Override
+        public Object instantiateItem(View container, int position) {
+            Log.d(TAG, "instantiateItem at position " + position);
+
+            LayoutInflater inflater = activity.getLayoutInflater();
+            View view = inflater.inflate(tabLayouts.get(position), null);
+
+            ((ViewPager) container).addView(view, 0);
+
+            return view;
+        }
+
+        @Override
+        public void destroyItem(View container, int position, Object view) {
+            Log.d(TAG, "destroyItem at position " + position);
+
+            if (currentTransaction == null) {
+                currentTransaction = fragmentManager.beginTransaction();
+            }
+
+            // XXX
+            switch (position) {
+            case 0:
+                Fragment fragment = fragmentManager
+                        .findFragmentById(R.id.dictionary_fragment);
+                if (fragment != null) {
+                    currentTransaction.remove(fragment);
+                }
+                removeFavoritesHistoryFragments();
+                break;
+            case 1:
+                fragment = fragmentManager
+                        .findFragmentById(R.id.kanji_lookup_fragment);
+                if (fragment != null) {
+                    currentTransaction.remove(fragment);
+                }
+                removeFavoritesHistoryFragments();
+                break;
+            case 2:
+                fragment = fragmentManager
+                        .findFragmentById(R.id.example_search_fragment);
+                if (fragment != null) {
+                    currentTransaction.remove(fragment);
+                }
+                removeFavoritesHistoryFragments();
+                break;
+            }
+
+            ((ViewPager) container).removeView((View) view);
+        }
+
+        private void removeFavoritesHistoryFragments() {
+            Fragment fragment = fragmentManager
+                    .findFragmentById(R.id.history_fragment);
+            if (fragment != null) {
+                currentTransaction.remove(fragment);
+            }
+            fragment = fragmentManager
+                    .findFragmentById(R.id.favorites_fragment);
+            if (fragment != null) {
+                currentTransaction.remove(fragment);
+            }
+        }
+
+        @Override
+        public void finishUpdate(View container) {
+            if (currentTransaction != null) {
+                currentTransaction.commit();
+                currentTransaction = null;
+                fragmentManager.executePendingTransactions();
+            }
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
+
+        @Override
+        public void restoreState(Parcelable state, ClassLoader loader) {
+        }
+    }
+
     private ViewPager viewPager;
-    private TabsPagerAdapter tabsAdapter;
+    //    private TabsPagerAdapter tabsAdapter;
+    private WwwjdicTabsPagerAdapter tabsAdapter;
+
 
     /** Called when the activity is first created. */
     @Override
@@ -159,9 +329,12 @@ public class Wwwjdic extends FragmentActivity {
             dictionaryTab.setText(R.string.dictionary);
         }
         viewPager = (ViewPager) findViewById(R.id.content);
-        tabsAdapter = new TabsPagerAdapter(this, getSupportActionBar(),
+        //        tabsAdapter = new TabsPagerAdapter(this, getSupportActionBar(),
+        //                viewPager);
+        tabsAdapter = new WwwjdicTabsPagerAdapter(this, getSupportActionBar(),
                 viewPager);
-        tabsAdapter.addTab(dictionaryTab, dictionary);
+        //        tabsAdapter.addTab(dictionaryTab, dictionary);
+        tabsAdapter.addTab(dictionaryTab, R.layout.dict_lookup_tab);
 
         ActionBar.Tab kanjiTab = getSupportActionBar().newTab();
         WwwjdicFragmentBase kanjiLookup = new KanjiLookupFragment();
@@ -173,7 +346,8 @@ public class Wwwjdic extends FragmentActivity {
         if (IS_HONEYCOMB) {
             kanjiTab.setText(R.string.kanji_lookup);
         }
-        tabsAdapter.addTab(kanjiTab, kanjiLookup);
+        //        tabsAdapter.addTab(kanjiTab, kanjiLookup);
+        tabsAdapter.addTab(kanjiTab, R.layout.kanji_lookup_tab);
 
         ActionBar.Tab examplesTab = getSupportActionBar().newTab();
         ExampleSearchFragment exampleSearch = new ExampleSearchFragment();
@@ -185,7 +359,8 @@ public class Wwwjdic extends FragmentActivity {
         if (IS_HONEYCOMB) {
             examplesTab.setText(R.string.example_search);
         }
-        tabsAdapter.addTab(examplesTab, exampleSearch);
+        //        tabsAdapter.addTab(examplesTab, exampleSearch);
+        tabsAdapter.addTab(examplesTab, R.layout.example_search_tab);
 
         getSupportActionBar().setSelectedNavigationItem(DICTIONARY_TAB_IDX);
         if (extras != null) {
