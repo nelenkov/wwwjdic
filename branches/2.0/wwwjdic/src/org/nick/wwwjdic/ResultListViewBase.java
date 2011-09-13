50457dcf4103433d5ecc08ce4db3a6bf584b8dda
nikolay.elenkov@gmail.com <nikolay.elenkov@gmail.com@74032484-283b-11df-b97d-a14e33d00e86>
2011-09-13 07:47:51 +0000

    use custom action bar style

--- branches/2.0/wwwjdic/src/org/nick/wwwjdic/ResultListViewBase.java
+++ branches/2.0/wwwjdic/src/org/nick/wwwjdic/ResultListViewBase.java
@@ -2,7 +2,9 @@ package org.nick.wwwjdic;
 
 import org.nick.wwwjdic.utils.Analytics;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.MenuItem;
 
 public abstract class ResultListViewBase extends FragmentActivity {
 
@@ -23,4 +25,16 @@ public abstract class ResultListViewBase extends FragmentActivity {
         Analytics.endSession(this);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            Intent intent = new Intent(this, Wwwjdic.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
