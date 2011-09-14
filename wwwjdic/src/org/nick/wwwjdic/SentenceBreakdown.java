ba1d9d491a80e0555389cc0bb79766f3119bd860
nikolay.elenkov@gmail.com <nikolay.elenkov@gmail.com@74032484-283b-11df-b97d-a14e33d00e86>
2011-09-14 09:12:38 +0000

    properly refresh on config change; handle back based on source activity

--- wwwjdic/src/org/nick/wwwjdic/SentenceBreakdown.java
+++ wwwjdic/src/org/nick/wwwjdic/SentenceBreakdown.java
@@ -1,10 +1,7 @@
 package org.nick.wwwjdic;
 
-import static org.nick.wwwjdic.Constants.EXAMPLE_SEARRCH_TAB_IDX;
-import static org.nick.wwwjdic.Constants.SELECTED_TAB_IDX;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.Menu;
 import android.support.v4.view.MenuItem;
 
 public class SentenceBreakdown extends ResultListViewBase {
@@ -12,11 +9,17 @@ public class SentenceBreakdown extends ResultListViewBase {
     public static final String EXTRA_SENTENCE = "org.nick.wwwjdic.SENTENCE";
     public static final String EXTRA_SENTENCE_TRANSLATION = "org.nick.wwwjdic.SENTENCE_TRANSLATION";
 
-    private static final int ITEM_ID_HOME = 0;
+    private boolean exampleBreakdown;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        String translation = getIntent().getExtras().getString(
+                EXTRA_SENTENCE_TRANSLATION);
+        if (translation != null) {
+            exampleBreakdown = true;
+        }
 
         setContentView(R.layout.sentence_breakdown);
     }
@@ -29,19 +32,12 @@ public class SentenceBreakdown extends ResultListViewBase {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        menu.add(0, ITEM_ID_HOME, 0, R.string.home).setIcon(
-                android.R.drawable.ic_menu_compass);
-
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
         case android.R.id.home:
-            Intent intent = new Intent(this, ExamplesResultListView.class);
+            Intent intent = new Intent(this,
+                    exampleBreakdown ? ExamplesResultListView.class
+                            : Wwwjdic.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
                     | Intent.FLAG_ACTIVITY_SINGLE_TOP);
             startActivity(intent);
@@ -51,25 +47,6 @@ public class SentenceBreakdown extends ResultListViewBase {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onMenuItemSelected(int featureId, MenuItem item) {
-        switch (item.getItemId()) {
-        case ITEM_ID_HOME:
-            Intent intent = new Intent(this, Wwwjdic.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            intent.putExtra(SELECTED_TAB_IDX, EXAMPLE_SEARRCH_TAB_IDX);
-
-            startActivity(intent);
-            finish();
-
-            return true;
-        default:
-            // do nothing
-        }
-
-        return super.onMenuItemSelected(featureId, item);
     }
 
 }
