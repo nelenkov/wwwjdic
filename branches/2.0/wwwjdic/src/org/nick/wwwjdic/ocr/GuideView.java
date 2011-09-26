fab797d65b1b84cd5158a7237727cd27dceb5b90
nikolay.elenkov@gmail.com <nikolay.elenkov@gmail.com@74032484-283b-11df-b97d-a14e33d00e86>
2011-09-26 14:03:56 +0000

    added transparent action bar to OCR on HC

--- branches/2.0/wwwjdic/src/org/nick/wwwjdic/ocr/GuideView.java
+++ branches/2.0/wwwjdic/src/org/nick/wwwjdic/ocr/GuideView.java
@@ -5,13 +5,16 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class GuideView extends View {
 
+    private static final boolean IS_HONEYCOMB = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
+
     private static int GUIDE_HEIGHT = 50;
-    private static int FIRST_GUIDE_Y = 50;
+    private static int FIRST_GUIDE_Y = IS_HONEYCOMB ? 58 : 0;
 
     public GuideView(Context context) {
         super(context);
@@ -23,13 +26,15 @@ public class GuideView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
+        float density = getResources().getDisplayMetrics().density;
+
         final Paint paint = new Paint();
         final Rect rect = new Rect();
 
         int width = canvas.getWidth();
         int height = canvas.getHeight();
 
-        int usedHeight = height - FIRST_GUIDE_Y;
+        int usedHeight = height - (int) (FIRST_GUIDE_Y * density);
         int numGuides = usedHeight / GUIDE_HEIGHT;
 
         int top = 0;
@@ -37,11 +42,16 @@ public class GuideView extends View {
         paint.setColor(Color.LTGRAY);
         for (int i = 0; i < numGuides; i++) {
             if (i == 0) {
-                top = FIRST_GUIDE_Y;
+                top = (int) (FIRST_GUIDE_Y * density);
             } else {
                 top = top + GUIDE_HEIGHT;
             }
-            rect.set(0, top, width, top + GUIDE_HEIGHT);
+
+            int bottom = top + GUIDE_HEIGHT;
+            if (i == numGuides - 1) {
+                bottom = height - 1;
+            }
+            rect.set(0, top, width, bottom);
             canvas.drawRect(rect, paint);
         }
     }
