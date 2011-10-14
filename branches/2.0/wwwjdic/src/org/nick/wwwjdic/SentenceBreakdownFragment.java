a7b437a52161f838aa36169ffe3d10edf9841c44
nikolay.elenkov@gmail.com <nikolay.elenkov@gmail.com@74032484-283b-11df-b97d-a14e33d00e86>
2011-10-14 08:59:24 +0000

    added speak menu to example breakdown

--- branches/2.0/wwwjdic/src/org/nick/wwwjdic/SentenceBreakdownFragment.java
+++ branches/2.0/wwwjdic/src/org/nick/wwwjdic/SentenceBreakdownFragment.java
@@ -215,14 +215,16 @@ public class SentenceBreakdownFragment extends
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-        case R.id.menu_context_example_copy_jp:
+        case R.id.menu_example_copy_jp:
             copyJapanese();
             return true;
-        case R.id.menu_context_example_copy_eng:
+        case R.id.menu_example_copy_eng:
             copyEnglish();
             return true;
-        case R.id.menu_context_example_lookup_kanji:
+        case R.id.menu_example_lookup_kanji:
             lookupAllKanji();
+            return true;
+        case R.id.menu_ex_breakdown_speak:
             return true;
         }
 
