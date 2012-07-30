91f42f90b59ced024633457f33df75ecf6fb31bc
nikolay.elenkov@gmail.com <nikolay.elenkov@gmail.com@74032484-283b-11df-b97d-a14e33d00e86>
2012-07-30 13:59:49 +0000

    https://github.com/JakeWharton/ActionBarSherlock/issues/240

--- branches/2.0/wwwjdic/src/org/nick/wwwjdic/history/TabsPagerAdapter.java
+++ branches/2.0/wwwjdic/src/org/nick/wwwjdic/history/TabsPagerAdapter.java
@@ -1,5 +1,8 @@
 package org.nick.wwwjdic.history;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,12 +13,16 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.ActionBar.Tab;
 
 public class TabsPagerAdapter extends FragmentPagerAdapter implements
         ViewPager.OnPageChangeListener, ActionBar.TabListener {
+
+    private static final String TAG = TabsPagerAdapter.class.getSimpleName();
 
     private FragmentActivity activity;
     private final ActionBar actionBar;
@@ -60,6 +67,10 @@ public class TabsPagerAdapter extends FragmentPagerAdapter implements
         // TODO -- refreshing kills action bar?
         // refresh(position);
         setTitle(position);
+
+        // for ABS#240
+        // https://github.com/JakeWharton/ActionBarSherlock/issues/240
+        selectInSpinnerIfPresent(position, true);
     }
 
     @SuppressWarnings("unused")
@@ -98,6 +109,67 @@ public class TabsPagerAdapter extends FragmentPagerAdapter implements
             activity.setTitle(R.string.favorites);
         } else {
             activity.setTitle(R.string.history);
+        }
+    }
+
+    /**
+     * Hack that takes advantage of interface parity between ActionBarSherlock
+     * and the native interface to reach inside the classes to manually select
+     * the appropriate tab spinner position if the overflow tab spinner is
+     * showing.
+     * 
+     * Related issues:
+     * https://github.com/JakeWharton/ActionBarSherlock/issues/240 and
+     * https://android-review.googlesource.com/#/c/32492/
+     * 
+     * @author toulouse@crunchyroll.com
+     */
+    private void selectInSpinnerIfPresent(int position, boolean animate) {
+        try {
+            View actionBarView = activity.findViewById(R.id.abs__action_bar);
+            if (actionBarView == null) {
+                int id = activity.getResources().getIdentifier("action_bar",
+                        "id",
+                        "android");
+                actionBarView = activity.findViewById(id);
+            }
+
+            Class<?> actionBarViewClass = actionBarView.getClass();
+            Field mTabScrollViewField = actionBarViewClass
+                    .getDeclaredField("mTabScrollView");
+            mTabScrollViewField.setAccessible(true);
+
+            Object mTabScrollView = mTabScrollViewField.get(actionBarView);
+            if (mTabScrollView == null) {
+                return;
+            }
+
+            Field mTabSpinnerField = mTabScrollView.getClass()
+                    .getDeclaredField("mTabSpinner");
+            mTabSpinnerField.setAccessible(true);
+
+            Object mTabSpinner = mTabSpinnerField.get(mTabScrollView);
+            if (mTabSpinner == null) {
+                return;
+            }
+
+            Method setSelectionMethod = mTabSpinner
+                    .getClass()
+                    .getSuperclass()
+                    .getDeclaredMethod("setSelection", Integer.TYPE,
+                            Boolean.TYPE);
+            setSelectionMethod.invoke(mTabSpinner, position, animate);
+
+        } catch (IllegalArgumentException e) {
+            Log.w(TAG, "TabsPagerAdapter.selectInSpinnerIfPresent()", e);
+        } catch (IllegalAccessException e) {
+            Log.w(TAG, "TabsPagerAdapter.selectInSpinnerIfPresent()", e);
+        } catch (NoSuchFieldException e) {
+            Log.w(TAG, "TabsPagerAdapter.selectInSpinnerIfPresent()", e);
+        } catch (NoSuchMethodException e) {
+            Log.w(TAG, "TabsPagerAdapter.selectInSpinnerIfPresent()", e);
+        } catch (InvocationTargetException e) {
+            Log.w(TAG, "TabsPagerAdapter.selectInSpinnerIfPresent()", e);
         }
     }
 
