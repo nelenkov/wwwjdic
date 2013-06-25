4a35a6c3d98c86259ce90e0e28d05b97f36e7dd6
nikolay.elenkov@gmail.com <nikolay.elenkov@gmail.com@74032484-283b-11df-b97d-a14e33d00e86>
2013-06-25 05:32:11 +0000

    don't crash if details pane does not exist

--- branches/2.0/wwwjdic/src/org/nick/wwwjdic/KanjiResultListFragment.java
+++ branches/2.0/wwwjdic/src/org/nick/wwwjdic/KanjiResultListFragment.java
@@ -47,7 +47,9 @@ public class KanjiResultListFragment extends ResultListFragmentBase<KanjiEntry>
         View detailsFrame = getActivity().findViewById(R.id.details);
         dualPane = detailsFrame != null
                 && detailsFrame.getVisibility() == View.VISIBLE;
-        detailsFrame.setVisibility(View.GONE);
+        if (detailsFrame != null) {
+            detailsFrame.setVisibility(View.GONE);
+        }
 
         if (entries != null) {
             // we are being re-created after rotation, use existing data
